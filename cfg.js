7dda1c7d61c03d75f7760e8f00655abd10e464d6
ouzhigang <ouzhigangGBA@gmail.com>
2016-10-21 15:51:13 +0800

    Update cfg.js

--- cfg.js
+++ cfg.js
@@ -1,7 +1,7 @@
 ﻿exports.WEB_NAME = "nodejs-ozgweb";
 exports.DB_PATH = "./db.sys"; //数据库
 exports.SERVER_PORT = 8000; //监听端口
-exports.JQUERY = "http://ajax.useso.com/ajax/libs/jquery/2.1.1/jquery.min.js"; //jquery2.0以上
+exports.JQUERY = "http://cdn.bootcss.com/jquery/2.2.4/jquery.min.js"; //jquery2.0以上
 exports.PAGE_SIZE = 16;
 
 exports.ADMIN_MENU_LIST = [
